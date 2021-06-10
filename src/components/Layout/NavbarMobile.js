730855e09ee869f083959d7fb1683d1211b1b046
rajeev-ranjan <rjeevrnjn17@gmail.com>
2021-06-10 14:38:23 +0530

    added menu to hamburger

--- src/components/Layout/NavbarMobile.js
+++ src/components/Layout/NavbarMobile.js
@@ -1,4 +1,14 @@
-import { AppBar, Container, Grid, IconButton } from "@material-ui/core";
+import {
+	AppBar,
+	Container,
+	Grid,
+	IconButton,
+	Backdrop,
+	ListItem,
+	Link,
+	List,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import React from "react";
 import {
 	basketIcon,
@@ -7,44 +17,82 @@ import {
 	searchIcon,
 } from "../../assets";
 import { CustomTypography } from "../Common/CustomTypography";
-const NavbarMobile = () => {
-	return (
-		<AppBar color='transparent' elevation={0}>
-			<Container>
-				<Grid container justify='space-between'>
-					<Grid item xs={3}>
-						<img src={homeJamLogo} alt='homejam logo' />
-					</Grid>
-					<Grid
-						container
-						item
-						xs={6}
-						justify='flex-end'
-						alignItems='center'
-						spacing={8}>
-						<Grid item>
-							<CustomTypography
-								variant='subtitle2'
-								color='primary'>
-								<img src={searchIcon} alt='search icon' />
-							</CustomTypography>
-						</Grid>
 
-						<Grid item>
-							<img src={basketIcon} alt='basket icon' />{" "}
+const backDropStyles = makeStyles((theme) => ({
+	backdrop: {
+		zIndex: theme.zIndex.drawer + 1,
+		color: "#fff",
+	},
+}));
+
+const NavbarMobile = () => {
+	const classes = backDropStyles();
+	const [open, setOpen] = React.useState(false);
+	const handleClose = () => {
+		setOpen(false);
+	};
+	const handleToggle = () => {
+		setOpen(!open);
+	};
+
+	return (
+		<>
+			<AppBar color='transparent' elevation={0}>
+				<Container>
+					<Grid container justify='space-between'>
+						<Grid item xs={3}>
+							<img src={homeJamLogo} alt='homejam logo' />
 						</Grid>
-						<Grid item>
-							<IconButton
-								color='primary'
-								aria-label='open nav menu'
-								onClick={(e) => console.log("u clicked")}>
-								<img src={hamburgerIcon} alt='hamburger icon' />
-							</IconButton>
+						<Grid
+							container
+							item
+							xs={6}
+							justify='flex-end'
+							alignItems='center'
+							spacing={6}>
+							<Grid item>
+								<CustomTypography
+									variant='subtitle2'
+									color='primary'>
+									<img src={searchIcon} alt='search icon' />
+								</CustomTypography>
+							</Grid>
+
+							<Grid item>
+								<img src={basketIcon} alt='basket icon' />
+							</Grid>
+							<Grid item>
+								<IconButton
+									color='primary'
+									aria-label='open nav menu'
+									onClick={handleToggle}>
+									<img
+										src={hamburgerIcon}
+										alt='hamburger icon'
+									/>
+								</IconButton>
+							</Grid>
 						</Grid>
 					</Grid>
-				</Grid>
-			</Container>
-		</AppBar>
+				</Container>
+			</AppBar>
+			<Backdrop
+				className={classes.backdrop}
+				open={open}
+				onClick={handleClose}>
+				<List>
+					<ListItem>
+						<Link>Home</Link>
+					</ListItem>
+					<ListItem>
+						<Link>Help</Link>
+					</ListItem>
+					<ListItem>
+						<Link>Account</Link>
+					</ListItem>
+				</List>
+			</Backdrop>
+		</>
 	);
 };
 
